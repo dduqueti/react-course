571465c672f66fa1b895a4ea59a3ee318aa4a7a8
Daniel Duque <danieljduque@gmail.com>
2021-04-17 22:01:10 -0500

    (pixel-art-app): Add ColorPicker

---
+++ pixel-drawing-app/src/components/ColorPicker.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Pixel from './Pixel'
+import Colors from '../Colors'
+
+export default props => {
+	return (
+		<div className="colorpicker">
+			{Colors.map((color, index) => {
+				return <Pixel key={index} background={color} />
+			})}
+		</div>
+	)
+}
--- pixel-drawing-app/src/index.js
+++ pixel-drawing-app/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Canvas from './components/Canvas'
+import ColorPicker from './components/ColorPicker'
 
 const App = () => {
   return(
     <div className="App">
+      <ColorPicker />
       <Canvas />
     </div>
   )
